99bcad3fdde483a79705ebcf5aaf78ab20685d36
Neraito <neraito.kawaranaide@gmail.com>
2021-10-19 21:54:11 +0900

    da ti zadolbal

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const fs = require('fs');
 const { Client, Collection, Intents } = require('discord.js');
-const { token } = require('./config.json');
 
 global.client = new Client({ intents: [Intents.FLAGS.GUILDS] });
 
